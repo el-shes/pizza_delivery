c78c8662f956a81fe4bff3748cda84c6114bcc0c
rrfpCSIR <shes980@gmail.com>
2022-08-25 20:50:55 +0300

    Initial commit

---
+++ pizza/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'pizza'
+urlpatterns = [
+    path('', views.home, name='home_page'),
+    path('order', views.order, name='create_order'),
+
+]
