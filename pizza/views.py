bb5a804447fd4f76868e691d63a5b3d075fa34ff
rrfpCSIR <shes980@gmail.com>
2022-08-25 23:15:29 +0300

    form system updated

--- pizza/views.py
+++ pizza/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import PizzaForm
+
 
 # Create your views here.
 
@@ -8,4 +10,14 @@ def home(request):
 
 
 def order(request):
-    return render(request, 'pizza/order.html')
+    if request.method == "POST":
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = "Thanks for order! Your %s %s and %s pizza is on it's way!" % (filled_form.cleaned_data['size'],
+                                                                                  filled_form.cleaned_data['topping1'],
+                                                                                  filled_form.cleaned_data['topping2'])
+            new_pizza_form = PizzaForm()
+            return render(request, 'pizza/order.html', {'pizzaform': new_pizza_form, 'note': note})
+    else:
+        form = PizzaForm()
+        return render(request, 'pizza/order.html', {'pizzaform': form})
