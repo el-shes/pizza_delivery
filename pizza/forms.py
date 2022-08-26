43b5db401b413f32e2587bb8837b553611135f35
rrfpCSIR <shes980@gmail.com>
2022-08-26 21:37:07 +0300

    widgets try out

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from .models import Pizza
 
-# class PizzaForm(forms.Form):
-#     size = forms.ChoiceField(label='Size', choices=[('Small', 'small'), ('Medium', 'medium'), ('Large', 'large')])
-#     topping1 = forms.CharField(label='Topping 1', max_length=100)
-#     topping2 = forms.CharField(label='Topping 2', max_length=100)
 
+class PizzaForm(forms.Form):
+    size = forms.ChoiceField(label='Size', choices=[('Small', 'small'), ('Medium', 'medium'), ('Large', 'large')])
+    toppings = forms.MultipleChoiceField(choices=[("pep", "peperoni"), ("cheese", "cheese"), ("olives", "olives")],
+                                         widget=forms.CheckboxSelectMultiple)
 
-class PizzaForm(forms.ModelForm):
-    class Meta:
-        model = Pizza
-        fields = ['size', 'topping1', 'topping2']
+# class PizzaForm(forms.ModelForm):
+#     class Meta:
+#         model = Pizza
+#         fields = ['size', 'topping1', 'topping2']
+#         labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2'}
+
